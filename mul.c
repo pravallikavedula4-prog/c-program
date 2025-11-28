a2c98d8774197f25352926eae849ca75c72c331c
pravallikavedula4-prog <pravallikavedula4@gmail.com>
2025-11-28 14:31:41 +0530

    Add multiplication program in mul.c

---
+++ mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, product;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    product = a * b;
+
+    printf("Product = %d\n", product);
+
+    return 0;
+}
