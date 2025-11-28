b6ad4d50adaecbd94ed3969ee9092869523b4e70
pravallikavedula4-prog <pravallikavedula4@gmail.com>
2025-11-28 14:49:53 +0530

    Add factorial calculation using a while loop

---
+++ factorial while.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    unsigned long long factorial = 1;
+    int i = 1;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Error! Factorial of a negative number doesn't exist.\n");
+    } else {
+        while (i <= n) {
+            factorial *= i;
+            i++;
+        }
+        printf("Factorial of %d = %llu\n", n, factorial);
+    }
+
+    return 0;
+}
+
+
