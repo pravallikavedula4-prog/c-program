975572b63ef583b1727ce93ab5b4de967d813d3b
pravallikavedula4-prog <pravallikavedula4@gmail.com>
2025-12-23 21:24:56 +0530

    Create Number Guessing Game.c

---
+++ Number Guessing Game.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main() {
+    int number, guess, attempts = 0;
+    srand(time(0)); 
+    number = rand() % 100 + 1; 
+    printf("Guess the number (1-100):\n");
+    do {
+        scanf("%d", &guess);
+        attempts++;
+        if(guess > number) printf("Too high!\n");
+        else if(guess < number) printf("Too low!\n");
+        else printf("Congratulations! You guessed in %d attempts.\n", attempts);
+    } while(guess != number);
+    return 0;
+}
