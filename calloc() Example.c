549ac16d7255f4444e7b57214f6b190d0a5d4f72
pravallikavedula4-prog <pravallikavedula4@gmail.com>
2025-12-23 21:12:22 +0530

    Create calloc() Example.c

---
+++ calloc() Example.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int n, i;
+    int *ptr;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    ptr = (int*) calloc(n, sizeof(int));
+    if (ptr == NULL) {
+        printf("Memory not allocated.\n");
+        return 1;
+    }
+    for(i = 0; i < n; i++) {
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &ptr[i]);
+    }
+    printf("Elements are: ");
+    for(i = 0; i < n; i++) {
+        printf("%d ", ptr[i]);
+    }
+    printf("\n");
+    free(ptr); 
+    return 0;
+}
